1de053128f7d11edb5029d0bdd4bea6c293b9008
Steffo <bjoekeldude@gmail.com>
2021-12-09 18:57:08 +0100

    adds namespace vectorfunctions

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,42 @@
 #include<algorithm>
 #include<random>
 
-void add_value(std::vector<int>& container){
-    int input{};
-    std::cout << "Which Integer should be added? ";
-    std::cin >> input;
-    container.push_back(input);
-}
+namespace vectorfunctions{
+    void add_value(std::vector<int>& container){
+        int input{};
+        std::cout << "Which Integer should be added? ";
+        std::cin >> input;
+        container.push_back(input);
+    }
 
-void print_first(std::vector<int>& container){
-    std::cout << *container.begin() << std::endl;
+    void print_first(std::vector<int>& container){
+        std::cout << *container.begin() << std::endl;
+    }
+    void print_last(std::vector<int>& container){
+        std::cout << container.back() << std::endl;
+    }
+    void print_all(std::vector<int>& container){
+        std::cout << "Print all the numbers!" << std::endl;
+        for(auto i: container) std::cout << i << ", ";
+        std::cout << std::endl;
+    }
+    void shuffle(std::vector<int>& container){
+        std::cout << "\033[1;41;30mShuffle!\033[0m" << std::endl;
+        auto rng = std::default_random_engine{};
+        std::shuffle(std::begin(container), std::end(container), rng);
+    }
+    void delete_last(std::vector<int>& container){
+        std::cout << "Throw away last element!" << std::endl;
+        container.pop_back();
+    }
+    void multiply(std::vector<int>& multiplicands){
+        int multiplier{};
+        std::cout << "Which Integer to multiply? ";
+        std::cin >> multiplier;
+        std::transform(multiplicands.begin(), multiplicands.end(), multiplicands.begin(),
+            [multiplier](int single_multiplicand)->int {return single_multiplicand * multiplier;});
+    }
 }
-void print_last(std::vector<int>& container){
-    std::cout << container.back() << std::endl;
-}
-void print_all(std::vector<int>& container){
-    std::cout << "Print all the numbers!" << std::endl;
-    for(auto i: container) std::cout << i << ", ";
-    std::cout << std::endl;
-}
-void shuffle(std::vector<int>& container){
-    std::cout << "\033[1;41;30mShuffle!\033[0m" << std::endl;
-    auto rng = std::default_random_engine{};
-    std::shuffle(std::begin(container), std::end(container), rng);
-}
-void delete_last(std::vector<int>& container){
-    std::cout << "Throw away last element!" << std::endl;
-    container.pop_back();
-}
-void multiply(std::vector<int>& multiplicands){
-    int multiplier{};
-    std::cout << "Which Integer to multiply? ";
-    std::cin >> multiplier;
-    std::transform(multiplicands.begin(), multiplicands.end(), multiplicands.begin(),
-        [multiplier](int single_multiplicand)->int {return single_multiplicand * multiplier;});
-}
-
 
 
 void print_dialog(){
@@ -68,13 +69,13 @@ void mainLoop(std::vector<int>& internal_container){
         std::cin >> option;
         switch(option){
             default:    std::cout << "unknown command" << std::endl;    break;
-            case 1:     add_value(internal_container);                  break;
-            case 2:     print_first(internal_container);                break;
-            case 3:     print_last(internal_container);                 break;
-            case 4:     print_all(internal_container);                  break;
-            case 5:     shuffle(internal_container);                    break;
-            case 6:     delete_last(internal_container);                break;
-            case 7:     multiply(internal_container);                   break;
+            case 1:     vectorfunctions::add_value(internal_container);                  break;
+            case 2:     vectorfunctions::print_first(internal_container);                break;
+            case 3:     vectorfunctions::print_last(internal_container);                 break;
+            case 4:     vectorfunctions::print_all(internal_container);                  break;
+            case 5:     vectorfunctions::shuffle(internal_container);                    break;
+            case 6:     vectorfunctions::delete_last(internal_container);                break;
+            case 7:     vectorfunctions::multiply(internal_container);                   break;
             case 0:     return;};
         };
 
